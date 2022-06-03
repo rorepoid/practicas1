8eb6f2d628718514a91c28c90cdd10e0ced92a08
Kevin Blancas <kevinblancasowo@gmail.com>
2022-06-03 00:22:26 -0500

    Initial commit

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('/css/login.css') }} ">
+    <link rel="stylesheet" href="{{ asset('/css/cabecera.css') }} ">
+</head>
+
+<body>
+    <div>
+        {{ $slot }}
+    </div>
+</body>
+
+</html>
