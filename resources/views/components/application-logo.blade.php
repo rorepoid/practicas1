292aa93e5792c8c2b2d650364b347bad65132948
Roberto Orellana <rorepoid@gmail.com>
2022-07-04 23:55:10 -0500

    Remove xml version tag

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,3 @@
-<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="250px" viewBox="0 0 3536 1424" enable-background="new 0 0 3536 1424" xml:space="preserve">  <image id="image0" width="3536" height="1424" x="0" y="0"
     href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAADdAAAAWQCAYAAADgUIe4AAABHGlDQ1BpY2MAACiRY2BgMnB0cXJl
